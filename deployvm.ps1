e34f8b489d22fe737bd62aef737468528a2ae9de
unknown <yahya.a.aden@gmail.com>
2022-05-19 13:30:05 +0200

    Updated deployvm.ps1 script

--- deployvm.ps1
+++ deployvm.ps1
@@ -1,8 +1,6 @@
 # This is the script to use for deployment of the VMs specified in the ARM templates in the template folder
 # Our resource group is already predefined in the CAFFortmu blueprint
 
-$getarg = $args[0]
-
 $ResourceGroup = "FortmuLtd-VNet-rg"
 $TemplateOne = ".\Templates\VMTemplate_01.json"
 $ParameterOne = ".\Templates\VMParameters_01.json"
@@ -12,18 +10,7 @@ $TemplateTwo = ".\Templates\VMTemplate_02.json"
 $ParameterTwo = ".\Templates\VMParameters_02.json"
 $DeploymentTwo = New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup -TemplateFile $TemplateTwo -TemplateParameterFile $ParameterTwo
 
+$DeploymentOne
+$DeploymentTwo
 
-# We check if the variable meets the criteria of our condition
-# If everything is correct, you will be asked to give an adminUsername and adminPassword for the VM
-# The new code is deployed after that
-
-if ($getarg -eq "deploy")
-{
-    $DeploymentOne
-    $DeploymentTwo
-}
-
-else
-{
-    Write-Host "Wrong deployment!" -ForegroundColor Red
-}
+Write-Host "Deployment complete!" -ForegroundColor Blue
