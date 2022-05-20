daefc31d00c8e7eb2006b14323cf9d7b527fa674
unknown <yahya.a.aden@gmail.com>
2022-05-20 11:58:44 +0200

    Update of deployvm.ps1

--- deployvm.ps1
+++ deployvm.ps1
@@ -6,9 +6,9 @@ $TemplateOne = ".\Templates\VMTemplate_01.json"
 $ParameterOne = ".\Templates\VMParameters_01.json"
 $DeploymentOne = New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup -TemplateFile $TemplateOne -TemplateParameterFile $ParameterOne
 
-$TemplateTwo = ".\Templates\VMTemplate_02.json"
-$ParameterTwo = ".\Templates\VMParameters_02.json"
-$DeploymentTwo = New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup -TemplateFile $TemplateTwo -TemplateParameterFile $ParameterTwo
+#$TemplateTwo = ".\Templates\VMTemplate_02.json"
+#$ParameterTwo = ".\Templates\VMParameters_02.json"
+#$DeploymentTwo = New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup -TemplateFile $TemplateTwo -TemplateParameterFile $ParameterTwo
 
 $DeploymentOne
-$DeploymentTwo
+#$DeploymentTwo
